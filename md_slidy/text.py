5571129a6f902f041e18746273c4726f5d06aff2
hkxyli <hkxyli@gmail.com>
2012-12-18 13:59:58 +0800

    1st Commit

---
+++ md_slidy/text.py
@@ -0,0 +1,12 @@
+import markdown
+import codecs
+
+# input_file=codecs.open("input.txt","r",encoding="utf-8")
+# text=input_file.read()
+# html=markdown.markdown(text,["tree"])
+# output_file=codecs.open("output.html","w",encoding="utf-8",errors="xmlcharrefreplace")
+# output_file.write(html)
+
+# md=markdown.Markdown()
+# html=md.convertFile(input="input.txt",output="output.html",encoding="utf-8")
+
